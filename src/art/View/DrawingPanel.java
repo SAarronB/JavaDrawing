ed2bf95341f1a0c786784f925229db1b2a55adf1
SAaronB <programmingsean@gmail.com>
2019-05-07 08:50:14 -0600

    New Data Members

--- src/art/View/DrawingPanel.java
+++ src/art/View/DrawingPanel.java
@@ -2,6 +2,7 @@ package art.View;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -11,17 +12,34 @@ import art.Controller.ArtController;
 
 public class DrawingPanel extends JPanel{
 	private ArtController app;
-	
+	private ArtPanel canvas;
+	private JPanel colorPanel;
+	private JPanel menuPanel;
+	private JScrollPane canvasPane;
+	private SpringLayout appLayout;
+	private JSlider widthSlider;
 	private JButton redButton;
+	private JButton saveButton;
 	private JButton blueButton;
 	private JButton orangeButton;
 	private JButton yellowButton;
 	private JButton greenButton;
 	private JButton violetButton;
 	private JButton magentaButton;
+	private JButton clearButton;
+	
+	
+	
+	setUpMenuPanels();
+	setupListeners();
+	setUpSlider();
+	setUpScrollPane();
+	setUpLayout();
 	
 	public DrawingPanel(ArtController app) {
 		super();
+		saveButton = new JButton("Save Panel");
+		
 		redButton = new JButton("Red");
 		blueButton = new JButton("Blue");
 		orangeButton = new JButton("orange");
